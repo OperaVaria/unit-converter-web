6a301ebda2b7f51b5a7e84b89619188b3bfd9aa3
OperaVaria <lcs_it@proton.me>
2024-02-19 15:59:34 +0100

    Code reformated

--- static/js/mainScript.js
+++ static/js/mainScript.js
@@ -5,16 +5,16 @@ const yearLocation = document.getElementById("year-location");
 
 // Insert current year into copyright info if element exists.
 if (document.body.contains(yearLocation)) {
-    let currentYear = new Date().getFullYear();
-    yearLocation.innerHTML = currentYear;
+  let currentYear = new Date().getFullYear();
+  yearLocation.innerHTML = currentYear;
 }
 
 // Load a page, same tab.
 function loadPage(Page) {
-    window.location.href = Page;
+  window.location.href = Page;
 }
 
 // Load a page in new tab.
 function loadNewTab(Page) {
-    window.open(Page, "_blank");
+  window.open(Page, "_blank");
 }
